d20137e5645faba89991a37ce8f7e7ccab4fb734
Philip Cramer <107579314+PhilipCramer@users.noreply.github.com>
2025-05-14 23:17:32 +0200

    updated talos cluster

--- Talos-Cluster/disk_image.tf
+++ Talos-Cluster/disk_image.tf
@@ -1,6 +1,6 @@
 locals {
   talos = {
-    version = "v1.9.4"
+    version = "v1.10.1"
   }
 }
 
@@ -10,7 +10,7 @@ resource "proxmox_virtual_environment_download_file" "talos_nocloud_image" {
   node_name    = "pve03"
 
   file_name               = "talos-${local.talos.version}-nocloud-amd64.img"
-  url                     = "https://factory.talos.dev/image/0d4ffb8ade43a79a8960e60a41c6fac75df764f253d572c0204535c1c6d2b828/${local.talos.version}/nocloud-amd64.raw.gz"
+  url                     = "https://factory.talos.dev/image/077514df2c1b6436460bc60faabc976687b16193b8a1290fda4366c69024fec2/${local.talos.version}/nocloud-amd64.raw.gz"
   decompression_algorithm = "gz"
   overwrite               = false
 }
